2e502dbd998b3c175c3636b600b6f76fb785595c
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-13 18:01:54 -0400

    :sparkles: feat: Take one user by his ID using /id on the end of localhost

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -9,6 +9,19 @@ class UserController {
             return res.status(500).json(error.message)
         }
     }
+    static async takeOneUser(req, res) {
+        const { id } = req.params
+        try{
+            const oneUser = await database.Users.findOne({
+                where:{
+                    id:Number(id)
+                }
+            })
+            return res.status(200).json(oneUser)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 }
 
 module.exports = UserController
