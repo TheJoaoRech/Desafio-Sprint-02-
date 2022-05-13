f559481cd04e5889cd94ed9097cdfae8372fd0c0
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-13 18:03:08 -0400

    :sparkles: feat: Adding a new route to get just one user by his ID

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -4,5 +4,6 @@ const UserController = require('../controllers/UserController')
 const router = Router()
 
 router.get('/api/v1/wallet', UserController.takeAllUsers)
+router.get('/api/v1/wallet/:id', UserController.takeOneUser)
 
 module.exports = router
