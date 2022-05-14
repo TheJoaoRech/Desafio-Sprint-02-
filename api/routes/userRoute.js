b79e10fd3e624ac517f2230e71ce8e3b662dc36d
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-14 00:35:02 -0400

    :sparkles: feat: Adding routes to delete and change a user information

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -6,5 +6,7 @@ const router = Router()
 router.get('/api/v1/wallet', UserController.takeAllUsers)
 router.get('/api/v1/wallet/:id', UserController.takeOneUser)
 router.post('/api/v1/wallet', UserController.CreateUser)
+router.put('/api/v1/wallet/:id', UserController.UpdateUser)
+router.delete('/api/v1/wallet/:id', UserController.DeleteUser)
 
 module.exports = router
