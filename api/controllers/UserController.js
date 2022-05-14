a8c5b4e05f498986be7314368c7f5588656c0cf1
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-14 00:34:15 -0400

    :sparkles: feat: Adding a DeleteUser and UpdateUser

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -32,6 +32,30 @@ class UserController {
             return res.status(500).json(error.message)
         }
     }
+
+    static async UpdateUser(req, res) {
+        const { id } = req.params 
+        const newInformations = req.body
+        try{
+            await database.Users.update(newInformations, { where: { id: Number(id) }})
+            const newInformationsChange = await database.Users.findOne({where:{
+                id:Number(id) }})
+                return res.status(200).json(newInformationsChange)
+        } catch (error){
+            return res.status(400).json(error.message)
+        }
+    }
+
+    static async DeleteUser(req, res) {
+        const { id } = req.params
+        try{
+            await database.Users.destroy({where:{
+                id:Number(id) }})
+                return res.status(204).json({})
+        } catch (error) {
+            return res.status(400).json(error.message)
+        }
+    }
 }
 
 module.exports = UserController
