fe71a05f22cd6644b98e12e5decfc8c1df3ccee3
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-14 17:49:05 -0400

    :truck: chore: Change to english the message server running

--- api/index.js
+++ api/index.js
@@ -6,6 +6,6 @@ const port = 3000
 
 routes(app)
 
-app.listen(port, () => console.log(`The server is running on the port ${port}!`))
+app.listen(port, () => console.log(`The server is running on the port: ${port}!`))
 
 module.exports = app
