da4999ad3932e3c7ca3c6016fa825f52703f3c4a
João <102974111+TheJoaoRech@users.noreply.github.com>
2022-05-14 22:26:12 -0400

    :sparkles: feat: Create a database for the coins

---
+++ api/migrations/20220515022223-create-coins.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Coins', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      coins: {
+        type: Sequelize.STRING
+      },
+      fullname: {
+        type: Sequelize.STRING
+      },
+      amont: {
+        type: Sequelize.FLOAT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize)
+   {
+    await queryInterface.dropTable('Coins');
+  }
+};
